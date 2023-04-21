94f1f230c97121f64595a3cb599eaf9aa6a6a1df
mat-bloch2 <48055190+mat-bloch2@users.noreply.github.com>
2023-04-21 13:15:32 +0200

    Dodaj pliki projektów.

---
+++ dragdrop/ListBoxExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace dragdrop
+{
+    public static class ListBoxExtension
+    {
+        public static ListBoxItem GetItemAt(this ListBox listBox,Point point)
+        {
+            DependencyObject zdażenie = VisualTreeHelper.HitTest(listBox, point).VisualHit;
+            while(zdażenie!=null && !(zdażenie is ListBoxItem))
+            zdażenie = VisualTreeHelper.GetParent(zdażenie);
+            return (ListBoxItem)zdażenie;
+        }
+
+      
+    }
+}
---
+++ dragdrop/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dragdrop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+           ListBox list= sender as ListBox;
+           ListBoxItem item= list.GetItemAt(e.GetPosition(list));
+            if(item!=null)
+            {
+                DataObject data = new DataObject();
+                data.SetData("lista", list);
+                data.SetData("item", item);
+                DragDrop.DoDragDrop(list, data, DragDropEffects.Move | DragDropEffects.Copy);
+             
+            }
+        }
+
+        private void ListBox_Drop(object sender, DragEventArgs e)
+        {
+
+            ListBox list = sender as ListBox;
+
+            ListBoxItem  dane = e.Data.GetData("item") as ListBoxItem;
+            ListBox oldlist = e.Data.GetData("lista") as ListBox;
+            oldlist.Items.Remove(dane);
+
+            list.Items.Add(dane);
+
+        }
+    }
+}
